4607314a796fa0b479a5df0710a6a7220e1fbde4
Cansu Öztaş <fcansuoztas@gmail.com>
2022-11-18 02:49:55 +0300

    Task

---
+++ comment.js
@@ -0,0 +1,131 @@
+
+const template = document.createElement("template");
+
+template.innerHTML = `
+    <link rel="stylesheet" href="comment.css" />
+    <div class="comment-box">
+        <div class="comment-edit">
+            <input class="comment-input" type="text" />
+            <button class="submit-btn">Submit</button>
+        </div>
+        <div class="comment-display">
+            <p class="comment">Comment</p>
+            <p class="author">Author:</p>
+            <p class="likes">Likes: 0</p>
+            <button class="like-btn">Like👍</button>
+            <button class="reply-btn">Reply📝</button>
+            <div class="reply-box"></div>
+        </div>
+    </div>
+`;
+
+/**Yanıt sayısı 4 ile sınırlıdır */
+const nestingLimit = 4;
+
+export class comment extends HTMLElement {
+    constructor() {
+    
+        super();
+        this.level = this.getAttribute("level")
+            ? 
+              parseInt(this.getAttribute("level"))
+            :
+              0;
+        this.likeCount = 0;
+
+    
+
+        this.attachShadow({ mode: "open" });
+
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+        this.commentEdit = this.shadowRoot.querySelector(".comment-edit");
+        this.commentDisplay = this.shadowRoot.querySelector(".comment-display");
+    }
+  
+    connectedCallback() {
+        this.commentEdit
+            .querySelector(".submit-btn")
+            .addEventListener("click", () => this.commentSubmit());
+
+       
+        this.commentDisplay
+            .querySelector(".like-btn")
+            .addEventListener("click", () => this.commentLike());
+
+    
+        if (this.level < nestingLimit) {
+            this.commentDisplay
+                .querySelector(".reply-btn")
+                .addEventListener("click", () => this.commentReply());
+        } else {
+           
+            this.commentDisplay.querySelector(".reply-btn").disabled = true;
+        }
+
+        this.commentDisplay.style.display = "none";
+    }
+
+  
+    commentSubmit() {
+   
+        const commentInput = this.commentEdit.querySelector(".comment-input");
+
+ 
+        const comment = this.commentDisplay.querySelector(".comment");
+
+        
+        const author = this.commentDisplay.querySelector(".author");
+
+       
+        comment.innerHTML = commentInput.value;
+    
+        author.innerHTML = `Author: ${sessionStorage.getItem("currentUser")}`;
+
+      
+        this.commentEdit.style.display = "none";
+     
+        this.commentDisplay.style.display = "block";
+    }
+
+   
+    commentLike() {
+      
+        const likes = this.commentDisplay.querySelector(".likes");
+
+       
+        this.likeCount++;
+
+        likes.innerHTML = `Likes: ${this.likeCount}`;
+    }
+
+   
+    commentReply() {
+  
+        const replyBox = this.commentDisplay.querySelector(".reply-box");
+       
+        const newcomment= document.createElement("comment-box");
+
+      
+        let newLevel = this.level + 1;
+
+        newcomment.setAttribute("level", newLevel);
+        newcomment.level = newLevel;
+
+      
+        if (replyBox.childNodes) {
+            
+            replyBox.insertBefore(newcomment, replyBox.childNodes[0]);
+        } else {
+           
+            replyBox.appendChild(newcomment);
+        }
+    }
+
+
+    disconnectedCallback() {
+        this.commentEdit.querySelector(".comment-submit").removeEventListener();
+        this.commentDisplay.querySelector(".reply-btn").removeEventListener();
+        this.commentDisplay.querySelector(".like-btn").removeEventListener();
+    }
+}
